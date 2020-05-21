b302a7efe5616548f4acc86efbd48c9aa861d120
Westley Feller <feller.westley@gmail.com>
2020-05-20 17:25:51 -0700

    Moved useResizeObserver to hooks folder Removed resetState
    Not necessary for the bar graph
    Formatting
    Fixed reset button and racing graph dates
    
    Daylight savings time was breaking animation

--- src/features/landing/footer/Footer.js
+++ src/features/landing/footer/Footer.js
@@ -5,162 +5,162 @@ import Img from "react-cool-img";
 import EarthImg from "./earthImg.svg";
 
 const useStyles = makeStyles({
-    Background: {
-        width: "100%",
-        display: "flex",
-        height: "100vh",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-    },
+  Background: {
+    width: "100%",
+    display: "flex",
+    height: "100vh",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+  },
 
-    footerBackground: {
-        background: "white",
-        width: "100%",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-        textAlign: "center",
-        height: "100vh",
-    },
+  footerBackground: {
+    background: "white",
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    textAlign: "center",
+    height: "100vh",
+  },
 
-    infoBox: {
-        position: "relative",
-        borderRadius: "10px",
-        background: "white",
-        height: "52%",
-        width: "40%",
-        opacity: 0.8,
-    },
+  infoBox: {
+    position: "relative",
+    borderRadius: "10px",
+    background: "white",
+    height: "52%",
+    width: "40%",
+    opacity: 0.8,
+  },
 
-    infoText: {
-        display: "flex",
-        flexDirection: "column",
-        // textAlign: 'center',
-        position: "absolute",
-        // padding: '100px',
-        // marginLeft: '25px',
-        // marginTop: '18px',
-    },
+  infoText: {
+    display: "flex",
+    flexDirection: "column",
+    // textAlign: 'center',
+    position: "absolute",
+    // padding: '100px',
+    // marginLeft: '25px',
+    // marginTop: '18px',
+  },
 
-    backgroungImg: {
-        height: "95%",
-        width: "100%",
-        position: "absolute",
-        paddingTop: "30px",
-    },
+  backgroungImg: {
+    height: "95%",
+    width: "100%",
+    position: "absolute",
+    paddingTop: "30px",
+  },
 
-    headText: {
-        // marginTop: '3.75rem',
-        paddingBottom: "30px",
-        color: "Black",
-    },
+  headText: {
+    // marginTop: '3.75rem',
+    paddingBottom: "30px",
+    color: "Black",
+  },
 
-    midText: {
-        paddingBottom: "30px",
-        color: "Black",
-    },
+  midText: {
+    paddingBottom: "30px",
+    color: "Black",
+  },
 
-    buttonsBox: {
-        width: "100%",
-        display: "flex",
-        justifyContent: "space-around",
-    },
+  buttonsBox: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-around",
+  },
 
-    buttons: {
-        fontSize: "12px",
-        backgroundColor: "#3EB6B4",
-        borderRadius: "60px",
-        height: "100%",
-        width: "100%",
-        color: "white",
-    },
+  buttons: {
+    fontSize: "12px",
+    backgroundColor: "#3EB6B4",
+    borderRadius: "60px",
+    height: "100%",
+    width: "100%",
+    color: "white",
+  },
 
-    bottomBox: {
-        width: "90%",
-        display: "flex",
-        justifyContent: "flex-end",
-        height: "10%",
-        background: "white",
-        paddingTop: "3rem",
-        paddingBottom: "2rem",
-    },
+  bottomBox: {
+    width: "90%",
+    display: "flex",
+    justifyContent: "flex-end",
+    height: "10%",
+    background: "white",
+    paddingTop: "3rem",
+    paddingBottom: "2rem",
+  },
 
-    tag: {
-        textDecoration: "none",
-    },
+  tag: {
+    textDecoration: "none",
+  },
 });
 
 const Footer = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <Box className={classes.Background}>
-            <Box className={classes.footerBackground}>
-                <Img
-                    src={EarthImg}
-                    className={classes.backgroungImg}
-                    alt="Eastern United States lights from space"
-                />
-                <Box className={classes.infoBox} />
-                <Box className={classes.infoText}>
-                    <Typography
-                        className={classes.headText}
-                        pt="8"
-                        variant="h5"
-                        component="p"
-                    >
-                        Want to learn more about <br /> Covid-19?
+  return (
+    <Box className={classes.Background}>
+      <Box className={classes.footerBackground}>
+        <Img
+          src={EarthImg}
+          className={classes.backgroungImg}
+          alt="Eastern United States lights from space"
+        />
+        <Box className={classes.infoBox} />
+        <Box className={classes.infoText}>
+          <Typography
+            className={classes.headText}
+            pt="8"
+            variant="h5"
+            component="p"
+          >
+            Want to learn more about <br /> Covid-19?
           </Typography>
 
-                    <Typography className={classes.midText} variant="h6" component="p">
-                        Check out the resources below.
+          <Typography className={classes.midText} variant="h6" component="p">
+            Check out the resources below.
           </Typography>
-                    <Box className={classes.buttonsBox}>
-                        <a
-                            className={classes.tag}
-                            style={{ color: "white", width: "45%" }}
-                            href="https://www.who.int/"
-                        >
-                            <Button
-                                href="https://www.who.int/"
-                                className={classes.buttons}
-                                style={{ color: "white" }}
-                                component="p"
-                            >
-                                World Health <br /> Organization: Who
+          <Box className={classes.buttonsBox}>
+            <a
+              className={classes.tag}
+              style={{ color: "white", width: "45%" }}
+              href="https://www.who.int/"
+            >
+              <Button
+                href="https://www.who.int/"
+                className={classes.buttons}
+                style={{ color: "white" }}
+                component="p"
+              >
+                World Health <br /> Organization: Who
               </Button>
-                        </a>
+            </a>
 
-                        <a
-                            className={classes.tag}
-                            style={{ color: "white", width: "45%" }}
-                            href="https://www.cdc.gov/coronavirus/2019-ncov/index.html"
-                        >
-                            <Button className={classes.buttons} component="p">
-                                CDC
+            <a
+              className={classes.tag}
+              style={{ color: "white", width: "45%" }}
+              href="https://www.cdc.gov/coronavirus/2019-ncov/index.html"
+            >
+              <Button className={classes.buttons} component="p">
+                CDC
               </Button>
-                        </a>
-                    </Box>
-                </Box>
-            </Box>
-            <Box className={classes.bottomBox}>
-                <Typography component="p" style={{ paddingRight: "35%" }}>
-                    &#169; PlanetData 2020
-        </Typography>
-                <Typography component="p">
-                    <a
-                        className={classes.tag}
-                        style={{ color: "black" }}
-                        href="#what-is-planet-data"
-                    >
-                        Back To Top
-          </a>
-                </Typography>
-            </Box>
+            </a>
+          </Box>
         </Box>
-    );
+      </Box>
+      <Box className={classes.bottomBox}>
+        <Typography component="p" style={{ paddingRight: "35%" }}>
+          &#169; PlanetData 2020
+        </Typography>
+        <Typography component="p">
+          <a
+            className={classes.tag}
+            style={{ color: "black" }}
+            href="#what-is-planet-data"
+          >
+            Back To Top
+          </a>
+        </Typography>
+      </Box>
+    </Box>
+  );
 };
 
 export default Footer;
--- src/features/visualizations/Racing-Chart/RacingData.js
+++ src/features/visualizations/Racing-Chart/RacingData.js
@@ -1,126 +1,150 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { add, compareAsc, parseISO } from "date-fns";
+import { add, getDayOfYear, parseISO } from "date-fns";
 import { Box, Button, Typography } from "@material-ui/core";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import RacingBarChart from "./RacingBarChart";
 import useInterval from "../../../hooks/useInterval";
-import { getConfirmedCases, resetStateHandler } from "./RacingSlice";
+import { getConfirmedCases } from "./RacingSlice";
 
 const useStyles = makeStyles({
-    ChartBox: {
-        height: "100vh",
-        width: "100%",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-    },
+  ChartBox: {
+    height: "100vh",
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+  },
 
-    titleText: {
-        textAlign: 'center',
-        padding: '.7rem'
-    },
+  titleText: {
+    textAlign: "center",
+    padding: ".7rem",
+  },
 
-    explanation: {
-        width: "75%",
-        paddingBottom: "4rem",
-        margin: "1rem auto 0",
-    },
+  explanation: {
+    width: "75%",
+    paddingBottom: "4rem",
+    margin: "1rem auto 0",
+  },
 
-    buttonBox: {
-        display: 'flex',
-        justifyContent: 'space-around',
-        width: '30%'
-    },
+  buttonBox: {
+    display: "flex",
+    justifyContent: "space-around",
+    width: "30%",
+  },
 
-    buttons: {
-        height: "2.5rem",
-        width: "10rem",
-        fontSize: "12px",
-        backgroundColor: "#3EB6B4",
-        borderRadius: "60px",
-        marginTop: '.5rem',
-        color: "white",
-    },
+  buttons: {
+    height: "2.5rem",
+    width: "10rem",
+    fontSize: "12px",
+    backgroundColor: "#3EB6B4",
+    borderRadius: "60px",
+    marginTop: ".5rem",
+    color: "white",
+  },
 });
 
 function RacingData() {
-    const dispatch = useDispatch();
-    const { deaths, fetching } = useSelector((state) => state.racingReducer);
-    const [start, setStart] = useState(false);
-    const [reset, setReset] = useState(null);
-    const [data, setData] = useState(null);
-    const [dateToFilter, setDateToFilter] = useState(null);
-    const classes = useStyles();;
+  const dispatch = useDispatch();
+  const { deaths, fetching } = useSelector((state) => state.racingReducer);
+  const [start, setStart] = useState(false);
+  const [data, setData] = useState(null);
+  const [dateToFilter, setDateToFilter] = useState(null);
+  const classes = useStyles();
 
-    useEffect(() => {
-        if (deaths !== null) {
-            setData(
-                deaths
-                    .map((x) => ({
-                        name: x.country,
-                        deaths: x.deaths,
-                        date: x.date,
-                    }))
-                    .filter((x) => x.date === deaths[0].date)
-            );
-            setDateToFilter(new Date(deaths[0].date));
-        }
-    }, [deaths]);
-
-    useInterval(() => {
-        if (start) {
-            setData(
-                deaths
-                    .map((x) => ({
-                        name: x.country,
-                        deaths: x.deaths,
-                        date: x.date,
-                    }))
-                    .filter((x) => compareAsc(parseISO(x.date), dateToFilter) === 0)
-            );
-            setDateToFilter(add(dateToFilter, { days: 1 }));
-        }
-    }, 200);
-
-    useEffect(() => {
-        dispatch(getConfirmedCases());
-    }, [dispatch]);
-
-    const handleClick = (e) => {
-        e.preventDefault();
-        dispatch(resetStateHandler())
+  useEffect(() => {
+    if (deaths !== null) {
+      setData(
+        deaths
+          .map((x) => ({
+            name: x.country,
+            deaths: x.deaths,
+            date: x.date,
+          }))
+          .filter((x) => x.date === deaths[0].date)
+      );
+      setDateToFilter(new Date(deaths[0].date));
     }
+  }, [deaths]);
 
+  // eslint-disable-next-line consistent-return
+  useInterval(() => {
+    if (start) {
+      setData(
+        deaths
+          .map((x) => ({
+            name: x.country,
+            deaths: x.deaths,
+            date: x.date,
+          }))
+          .filter(
+            (x) => getDayOfYear(parseISO(x.date)) === getDayOfYear(dateToFilter)
+          )
+      );
+      setDateToFilter(add(dateToFilter, { days: 1 }));
 
-    return (
-        <Box className={classes.ChartBox}>
-            <Box className={classes.titleText}>
-                <Typography variant="h4" component="h2" style={{ paddingTop: '1rem' }}>
-                    Explore COVID-19 Case Rates by Country
-                </Typography>
-                <Typography varient='h5'>
-                    Confirmed deaths (Covid-19)</Typography>
-            </Box>
-            <RacingBarChart data={!fetching && data} />
-            <Box className={classes.buttonBox}>
-                <Button className={classes.buttons} type="button" onClick={() => setStart(!start)}>
-                    {start ? "Stop the race" : "Start the race!"}
-                </Button>
-                <Button className={classes.buttons} type="button" onClick={e => handleClick(e)}>
-                    Reset race
-            </Button>
-            </Box>
-            <Typography className={classes.explanation}>
-                Even the most basic graphs like a bar chart can be engaging and interesting with the right set up and data.
-                Time-series data (data that has a date and/or time associated to it) allows us to see changes that happen over-time.
-                Here we are able to see the total death counts for the world countries that have the highest totals in a basic bar graph,
-                because we have time-series data we are able to
-                put the data in an animation from the very first day a COVID-19 death was confirmed through the present day.
-            </Typography>
-        </Box>
+      if (
+        getDayOfYear(parseISO(deaths[deaths.length - 1].date)) ===
+        getDayOfYear(dateToFilter)
+      ) {
+        return setStart(false);
+      }
+    }
+  }, 200);
+
+  useEffect(() => {
+    dispatch(getConfirmedCases());
+  }, [dispatch]);
+
+  const handleReset = (e) => {
+    e.preventDefault();
+    setStart(false);
+    setData(
+      deaths
+        .map((x) => ({
+          name: x.country,
+          deaths: x.deaths,
+          date: x.date,
+        }))
+        .filter((x) => x.date === deaths[0].date)
     );
+    setDateToFilter(new Date(deaths[0].date));
+  };
+
+  return (
+    <Box className={classes.ChartBox}>
+      <Box className={classes.titleText}>
+        <Typography variant="h4" component="h2" style={{ paddingTop: "1rem" }}>
+          Explore COVID-19 Case Rates by Country
+        </Typography>
+        <Typography varient="h5">Confirmed deaths (Covid-19)</Typography>
+      </Box>
+      <RacingBarChart data={!fetching && data} />
+      <Box className={classes.buttonBox}>
+        <Button
+          className={classes.buttons}
+          type="button"
+          onClick={() => setStart(!start)}
+        >
+          {start ? "Stop the race" : "Start the race!"}
+        </Button>
+        <Button className={classes.buttons} type="button" onClick={handleReset}>
+          Reset race
+        </Button>
+      </Box>
+      <Typography className={classes.explanation}>
+        Even the most basic graphs like a bar chart can be engaging and
+        interesting with the right set up and data. Time-series data (data that
+        has a date and/or time associated to it) allows us to see changes that
+        happen over-time. Here we are able to see the total death counts for the
+        world countries that have the highest totals in a basic bar graph,
+        because we have time-series data we are able to put the data in an
+        animation from the very first day a COVID-19 death was confirmed through
+        the present day.
+      </Typography>
+    </Box>
+  );
 }
 
 export default RacingData;
