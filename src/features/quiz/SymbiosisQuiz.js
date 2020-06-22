3871fa0a742963a4cb56e34d01281c816d29b957
Stephen Freeman <stephen.freeman.dev@gmail.com>
2020-06-21 18:21:38 -0700

    finish styling the quiz (finally)

--- src/features/quiz/SymbiosisQuiz.js
+++ src/features/quiz/SymbiosisQuiz.js
@@ -6,133 +6,180 @@ import Questions from "./questions/Questions";
 import Answers from "./answers/Answers";
 import { makeStyles } from "@material-ui/core/styles";
 import { Box, Typography, Button } from "@material-ui/core";
+import VisTitle from "../visualizations/VisTitle";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import HighlightOffIcon from "@material-ui/icons/HighlightOff";
+import { green, red } from "@material-ui/core/colors";
 
 const useStyles = makeStyles({
-  Container: {
-    width: "100%",
-    height: "70vh",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-  },
+    Container: {
+        width: "100%",
+        height: "70vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    ResultsContainer: {
+        width: "100%",
+        height: "50vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    correct: {
+        fontSize: '14px'
+    },
+    fontDisplay: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: "center",
+        alignItems: "center",
+        padding: '0'
+    }
 });
 
 export default function SymbiosisQuiz() {
-  const classes = useStyles();
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [currentAnswer, setCurrentAnswer] = useState("");
-  const [answers, setAnswers] = useState([]);
-  const [showResults, setShowResults] = useState(false);
-  const [error, setError] = useState([]);
+    const classes = useStyles();
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [currentAnswer, setCurrentAnswer] = useState("");
+    const [answers, setAnswers] = useState([]);
+    const [showResults, setShowResults] = useState(false);
+    const [error, setError] = useState([]);
 
-  const questions = [
-    {
-      id: 1,
-      question:
-        "On April 16th which US states contain the top 2 COVID - 19 hot spots?",
-      answer_a: "Zebra",
-      answer_b: "Algea",
-      answer_c: "Oxpecker bird",
-      answer_d: "Sea Anemone",
-      correct_answer: "c",
-    },
-  ];
+    const questions = [
+        {
+            id: 1,
+            question:
+                "Which animal has 3 relationships with other animals, including both mutualism and commensalism?",
+            answer_a: "Zebra",
+            answer_b: "Algea",
+            answer_c: "Oxpecker bird",
+            answer_d: "Sea Anemone",
+            correct_answer: "c",
+        },
+    ];
 
-  const question = questions[currentQuestion];
+    const question = questions[currentQuestion];
 
-  const handleClick = (e) => {
-    console.log("you clicked me", e.target.value);
-    setCurrentAnswer(e.target.value);
-    setError("");
-  };
+    const handleClick = (e) => {
+        console.log("you clicked me", e.target.value);
+        setCurrentAnswer(e.target.value);
+        setError("");
+    };
 
-  const renderError = () => {
-    if (!error) {
-      return;
+    const renderError = () => {
+        if (!error) {
+            return;
+        }
+        return <div className="error">{error}</div>;
+    };
+
+    const renderResultsMark = (questions, answer) => {
+        if (questions.correct_answer === answer.answer) {
+            return <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <span className="correct"><h3>Correct</h3></span>
+                <CheckCircleIcon style={{ color: green[500] }} />
+            </div>;
+        }
+
+        return <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '5.85rem' }}>
+            <span className="Failed"><h3>Failed</h3></span>
+            <HighlightOffIcon style={{ color: red[500] }} />
+        </div>;
+    };
+
+    const renderResultsData = () => {
+        return answers.map((answer) => {
+            const question = questions.find(
+                (question) => question.id === answer.questionId
+            );
+
+            return (
+                <VisTitle
+                    id="bubble-question-title"
+                    variant="h6"
+                    aria-label="bubble-title"
+                    key={question.id}
+                >
+                    <div className={classes.fontDisplay}>
+                        {question.question} {renderResultsMark(question, answer)}
+                    </div>
+                </VisTitle>
+            );
+        });
+    };
+
+    const restart = () => {
+        setAnswers([]);
+        setCurrentAnswer("");
+        setCurrentQuestion(0);
+        setShowResults(false);
+    };
+
+    const next = () => {
+        const answer = { questionId: question.id, answer: currentAnswer };
+
+        if (!currentAnswer) {
+            setError("Please select an option");
+            return;
+        }
+
+        answers.push(answer);
+        setAnswers(answers);
+        setCurrentAnswer("");
+
+        if (currentQuestion + 1 < questions.length) {
+            setCurrentQuestion(currentQuestion + 1);
+            return;
+        }
+
+        setShowResults(true);
+    };
+
+    if (showResults) {
+        return (
+            <Box className={classes.ResultsContainer}>
+                <VisTitle
+                    id="bubble-question-title"
+                    variant="h4"
+                    aria-label="bubble-title"
+                >
+                    Results
+            </VisTitle>
+                <ul style={{ padding: '0' }}>{renderResultsData()}</ul>
+                <Button
+                    style={{ width: "20%" }}
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    onClick={restart}
+                >
+                    Retry
+                </Button>
+            </Box>
+        );
+    } else {
+        return (
+            <Box className={classes.Container}>
+                <Progress total={questions.length} current={currentQuestion + 1} />
+                <Questions questions={question.question} />
+                {renderError()}
+                <Answers
+                    question={question}
+                    currentAnswer={currentAnswer}
+                    handleClick={handleClick}
+                />
+                <Button
+                    className="btn btn-primary"
+                    onClick={next}
+                    variant="contained"
+                    color="primary"
+                >
+                    Confirm
+                </Button>
+            </Box>
+        );
     }
-    return <div className="error">{error}</div>;
-  };
-
-  const renderResultsMark = (questions, answer) => {
-    if (questions.correct_answer === answer.answer) {
-      return <span className="correct">Correct</span>;
-    }
-
-    return <span className="Failed">Failed</span>;
-  };
-
-  const renderResultsData = () => {
-    return answers.map((answer) => {
-      const question = questions.find(
-        (question) => question.id === answer.questionId
-      );
-
-      return (
-        <div key={question.id}>
-          {question.question} - {renderResultsMark(question, answer)}
-        </div>
-      );
-    });
-  };
-
-  const restart = () => {
-    setAnswers([]);
-    setCurrentAnswer("");
-    setCurrentQuestion(0);
-    setShowResults(false);
-  };
-
-  const next = () => {
-    const answer = { questionId: question.id, answer: currentAnswer };
-
-    if (!currentAnswer) {
-      setError("Please select an option");
-      return;
-    }
-
-    answers.push(answer);
-    setAnswers(answers);
-    setCurrentAnswer("");
-
-    if (currentQuestion + 1 < questions.length) {
-      setCurrentQuestion(currentQuestion + 1);
-      return;
-    }
-
-    setShowResults(true);
-  };
-
-  if (showResults) {
-    return (
-      <Box className="container-results">
-        <h1>Results</h1>
-        <ul>{renderResultsData()}</ul>
-        <Button className="btn btn-primary" onClick={restart}>
-          Restart
-        </Button>
-      </Box>
-    );
-  } else {
-    return (
-      <Box className={classes.Container}>
-        <Progress total={questions.length} current={currentQuestion + 1} />
-        <Questions questions={question.question} />
-        {renderError()}
-        <Answers
-          question={question}
-          currentAnswer={currentAnswer}
-          handleClick={handleClick}
-        />
-        <Button
-          className="btn btn-primary"
-          onClick={next}
-          variant="contained"
-          color="primary"
-        >
-          Confirm
-        </Button>
-      </Box>
-    );
-  }
 }
--- src/features/visualizations/deforestation/income/CountryIncomeVis.js
+++ src/features/visualizations/deforestation/income/CountryIncomeVis.js
@@ -3,14 +3,14 @@ import { useDispatch, useSelector } from "react-redux";
 import useTheme from "@material-ui/core/styles/useTheme";
 import Box from "@material-ui/core/Box";
 import {
-  VictoryChart,
-  VictoryTheme,
-  VictoryLine,
-  VictoryAxis,
-  VictoryLegend,
-  VictoryVoronoiContainer,
-  VictoryArea,
-  VictoryTooltip,
+    VictoryChart,
+    VictoryTheme,
+    VictoryLine,
+    VictoryAxis,
+    VictoryLegend,
+    VictoryVoronoiContainer,
+    VictoryArea,
+    VictoryTooltip,
 } from "victory";
 import { schemeSet3 } from "d3";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
@@ -24,210 +24,210 @@ import { toggleShowSplash } from "../../../../app/theme/themeSlice";
 import IncomeQuiz from "../../../quiz/IncomeQuiz";
 
 export default withErrorBoundary(() => {
-  const dispatch = useDispatch();
-  const theme = useTheme();
-  const smallScreen = useMediaQuery(theme.breakpoints.down("sm"));
-  const mediumScreen = useMediaQuery(theme.breakpoints.up("md"));
-  const [windowWidth, windowHeight] = [
-    useWindowSize().width * 0.9,
-    useWindowSize().height,
-  ];
-  const [width, height] = [
-    mediumScreen ? windowWidth - theme.navBar.width : windowWidth,
-    smallScreen
-      ? (windowHeight - theme.appBar.height) * 0.8
-      : windowHeight * 0.8,
-  ];
+    const dispatch = useDispatch();
+    const theme = useTheme();
+    const smallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+    const mediumScreen = useMediaQuery(theme.breakpoints.up("md"));
+    const [windowWidth, windowHeight] = [
+        useWindowSize().width * 0.9,
+        useWindowSize().height,
+    ];
+    const [width, height] = [
+        mediumScreen ? windowWidth - theme.navBar.width : windowWidth,
+        smallScreen
+            ? (windowHeight - theme.appBar.height) * 0.8
+            : windowHeight * 0.8,
+    ];
 
-  const {
-    data,
-    error,
-    fetching,
-    data: { countryIncome },
-  } = useSelector((state) => state.countryIncomeReducer);
-  const { darkMode } = useSelector((state) => state.themeReducer);
+    const {
+        data,
+        error,
+        fetching,
+        data: { countryIncome },
+    } = useSelector((state) => state.countryIncomeReducer);
+    const { darkMode } = useSelector((state) => state.themeReducer);
 
-  useVisDataFetch("countryIncome", data, fetching, error);
+    useVisDataFetch("countryIncome", data, fetching, error);
 
-  useEffect(() => {
-    dispatch(toggleShowSplash());
-  }, [dispatch]);
+    useEffect(() => {
+        dispatch(toggleShowSplash());
+    }, [dispatch]);
 
-  if (fetching) {
-    return <LoadingSpinner />;
-  }
+    if (fetching) {
+        return <LoadingSpinner />;
+    }
 
-  return (
-    <Box
-      data-testid="vis-container"
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-    >
-      <VisTitle subtitled variant="h4" component="h2">
-        Deforestation Prediction Trends by Country Income 2019 - 2120
+    return (
+        <Box
+            data-testid="vis-container"
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+        >
+            <VisTitle subtitled variant="h4" component="h2">
+                Deforestation Prediction Trends by Country Income 2019 - 2120
       </VisTitle>
-      {countryIncome && (
-        <Box>
-          <VictoryChart
-            width={width}
-            height={height}
-            maxDomain={{ x: 2120, y: 40 }}
-            style={{
-              touchAction: "auto",
-            }}
-            padding={{
-              top: smallScreen ? 80 : 120,
-              right: smallScreen ? 30 : 80,
-              bottom: 60,
-              left: smallScreen ? 75 : 140,
-            }}
-            theme={VictoryTheme.material}
-            containerComponent={
-              <VictoryVoronoiContainer
-                voronoiBlacklist={["income-range"]}
-                labels={({ datum }) =>
-                  `${datum.x}: ${datum.incomeLevel} ${
-                    Math.ceil(datum.y * 100) / 100
-                  }%`
-                }
-                labelComponent={
-                  <VictoryTooltip
-                    style={{ fontSize: 14, fill: theme.palette.text.primary }}
-                    constrainToVisibleArea
-                    cornerRadius={theme.shape.borderRadius}
-                    flyoutStyle={{
-                      fill: theme.palette.background.paper,
-                      filter: "drop-shadow(0px 5px 5px rgba(0, 0, 0, .2))",
-                      stroke: 0,
-                    }}
-                  />
-                }
-              />
-            }
-          >
-            <VictoryArea
-              name="income-range"
-              data={[
-                {
-                  x: 2018,
-                  y: 0.1,
-                  y0: 40,
-                },
-                {
-                  x: 2120,
-                  y: 0.1,
-                  y0: 40,
-                },
-              ]}
-              style={{
-                data: {
-                  fill: darkMode ? schemeSet3[10] : schemeSet3[6],
-                  fillOpacity: 0.2,
-                  stroke: "none",
-                },
-              }}
-            />
-            <VictoryAxis
-              fixLabelOverlap
-              dependentAxis
-              style={{
-                tickLabels: {
-                  fill: theme.palette.text.primary,
-                  fontSize: smallScreen ? 14 : 20,
-                },
-                axisLabel: {
-                  fill: theme.palette.text.primary,
-                  fontSize: smallScreen ? 20 : 28,
-                  padding: smallScreen ? 45 : 60,
-                },
-                grid: {
-                  fill: `${theme.palette.text.hint}66`,
-                  stroke: `${theme.palette.text.hint}66`,
-                },
-              }}
-              tickFormat={(x) => `${x}%`}
-              label="Forest Area"
-            />
-            <VictoryAxis
-              fixLabelOverlap
-              scale="time"
-              style={{
-                tickLabels: {
-                  fill: theme.palette.text.primary,
-                  fontSize: smallScreen ? 14 : 20,
-                },
-                axisLabel: {
-                  fill: theme.palette.text.primary,
-                  fontSize: smallScreen ? 20 : 28,
-                  padding: smallScreen ? 32 : 36,
-                },
-                grid: {
-                  fill: `${theme.palette.text.hint}66`,
-                  stroke: `${theme.palette.text.hint}66`,
-                },
-              }}
-              tickFormat={(x) => x}
-              name="Year"
-              label="Year"
-            />
-            <VictoryLine
-              style={{
-                data: { stroke: schemeSet3[0], strokeWidth: 4.5 },
-              }}
-              data={countryIncome.filter(
-                (group) => group.incomeLevel === "High Income Countries"
-              )}
-            />
-            <VictoryLine
-              style={{
-                data: { stroke: schemeSet3[5], strokeWidth: 4.5 },
-              }}
-              data={countryIncome.filter(
-                (group) => group.incomeLevel === "Middle Income Countries"
-              )}
-            />
-            <VictoryLine
-              style={{
-                data: { stroke: schemeSet3[3], strokeWidth: 4.5 },
-              }}
-              data={countryIncome.filter(
-                (group) => group.incomeLevel === "Low Income Countries"
-              )}
-            />
-            <VictoryLegend
-              x={smallScreen ? 40 : 75}
-              y={smallScreen ? -10 : -5}
-              title="Country Income Level"
-              orientation="horizontal"
-              borderPadding={20}
-              style={{
-                title: {
-                  fontSize: smallScreen ? 16 : 20,
-                  fill: theme.palette.text.primary,
-                },
-                labels: {
-                  fontSize: smallScreen ? 14 : 20,
-                  fill: theme.palette.text.primary,
-                },
-                parent: { fill: "blue" },
-              }}
-              data={[
-                { name: "High", symbol: { fill: schemeSet3[0] } },
-                { name: "Middle", symbol: { fill: schemeSet3[5] } },
-                { name: "Low", symbol: { fill: schemeSet3[3] } },
-              ]}
-            />
-          </VictoryChart>
-        </Box>
-      )}
-      <VisExplanation>
-        Using the same prediction model and data source from the previous graph
-        we are able to group countries based on the country&apos;s income size.
-        This allows us see to how the forest area (% of land area) predictions
-        trend for high, medium, and low income countries.
+            {countryIncome && (
+                <Box>
+                    <VictoryChart
+                        width={width}
+                        height={height}
+                        maxDomain={{ x: 2120, y: 40 }}
+                        style={{
+                            touchAction: "auto",
+                        }}
+                        padding={{
+                            top: smallScreen ? 80 : 120,
+                            right: smallScreen ? 30 : 80,
+                            bottom: 60,
+                            left: smallScreen ? 75 : 140,
+                        }}
+                        theme={VictoryTheme.material}
+                        containerComponent={
+                            <VictoryVoronoiContainer
+                                voronoiBlacklist={["income-range"]}
+                                labels={({ datum }) =>
+                                    `${datum.x}: ${datum.incomeLevel} ${
+                                    Math.ceil(datum.y * 100) / 100
+                                    }%`
+                                }
+                                labelComponent={
+                                    <VictoryTooltip
+                                        style={{ fontSize: 14, fill: theme.palette.text.primary }}
+                                        constrainToVisibleArea
+                                        cornerRadius={theme.shape.borderRadius}
+                                        flyoutStyle={{
+                                            fill: theme.palette.background.paper,
+                                            filter: "drop-shadow(0px 5px 5px rgba(0, 0, 0, .2))",
+                                            stroke: 0,
+                                        }}
+                                    />
+                                }
+                            />
+                        }
+                    >
+                        <VictoryArea
+                            name="income-range"
+                            data={[
+                                {
+                                    x: 2018,
+                                    y: 0.1,
+                                    y0: 40,
+                                },
+                                {
+                                    x: 2120,
+                                    y: 0.1,
+                                    y0: 40,
+                                },
+                            ]}
+                            style={{
+                                data: {
+                                    fill: darkMode ? schemeSet3[10] : schemeSet3[6],
+                                    fillOpacity: 0.2,
+                                    stroke: "none",
+                                },
+                            }}
+                        />
+                        <VictoryAxis
+                            fixLabelOverlap
+                            dependentAxis
+                            style={{
+                                tickLabels: {
+                                    fill: theme.palette.text.primary,
+                                    fontSize: smallScreen ? 14 : 20,
+                                },
+                                axisLabel: {
+                                    fill: theme.palette.text.primary,
+                                    fontSize: smallScreen ? 20 : 28,
+                                    padding: smallScreen ? 45 : 60,
+                                },
+                                grid: {
+                                    fill: `${theme.palette.text.hint}66`,
+                                    stroke: `${theme.palette.text.hint}66`,
+                                },
+                            }}
+                            tickFormat={(x) => `${x}%`}
+                            label="Forest Area"
+                        />
+                        <VictoryAxis
+                            fixLabelOverlap
+                            scale="time"
+                            style={{
+                                tickLabels: {
+                                    fill: theme.palette.text.primary,
+                                    fontSize: smallScreen ? 14 : 20,
+                                },
+                                axisLabel: {
+                                    fill: theme.palette.text.primary,
+                                    fontSize: smallScreen ? 20 : 28,
+                                    padding: smallScreen ? 32 : 36,
+                                },
+                                grid: {
+                                    fill: `${theme.palette.text.hint}66`,
+                                    stroke: `${theme.palette.text.hint}66`,
+                                },
+                            }}
+                            tickFormat={(x) => x}
+                            name="Year"
+                            label="Year"
+                        />
+                        <VictoryLine
+                            style={{
+                                data: { stroke: schemeSet3[0], strokeWidth: 4.5 },
+                            }}
+                            data={countryIncome.filter(
+                                (group) => group.incomeLevel === "High Income Countries"
+                            )}
+                        />
+                        <VictoryLine
+                            style={{
+                                data: { stroke: schemeSet3[5], strokeWidth: 4.5 },
+                            }}
+                            data={countryIncome.filter(
+                                (group) => group.incomeLevel === "Middle Income Countries"
+                            )}
+                        />
+                        <VictoryLine
+                            style={{
+                                data: { stroke: schemeSet3[3], strokeWidth: 4.5 },
+                            }}
+                            data={countryIncome.filter(
+                                (group) => group.incomeLevel === "Low Income Countries"
+                            )}
+                        />
+                        <VictoryLegend
+                            x={smallScreen ? 40 : 75}
+                            y={smallScreen ? -10 : -5}
+                            title="Country Income Level"
+                            orientation="horizontal"
+                            borderPadding={20}
+                            style={{
+                                title: {
+                                    fontSize: smallScreen ? 16 : 20,
+                                    fill: theme.palette.text.primary,
+                                },
+                                labels: {
+                                    fontSize: smallScreen ? 14 : 20,
+                                    fill: theme.palette.text.primary,
+                                },
+                                parent: { fill: "blue" },
+                            }}
+                            data={[
+                                { name: "High", symbol: { fill: schemeSet3[0] } },
+                                { name: "Middle", symbol: { fill: schemeSet3[5] } },
+                                { name: "Low", symbol: { fill: schemeSet3[3] } },
+                            ]}
+                        />
+                    </VictoryChart>
+                </Box>
+            )}
+            <VisExplanation>
+                Using the same prediction model and data source from the previous graph
+                we are able to group countries based on the country&apos;s income size.
+                This allows us to see how the forest area (% of land area) predictions
+                trend for high, medium, and low income countries.
       </VisExplanation>
-      <IncomeQuiz />
-    </Box>
-  );
+            <IncomeQuiz />
+        </Box>
+    );
 }, "visualization");
